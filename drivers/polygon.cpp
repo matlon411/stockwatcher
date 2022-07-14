117225ec8368858724e3f69a262607f5ebc8fe65
matlon411 <mathias_lendal@hotmail.dk>
2022-07-14 16:38:59 +0200

    The start

---
+++ drivers/polygon.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+class PolygonRequest
+{
+private:
+    /* data */
+public:
+    PolygonRequest(/* args */);
+    ~PolygonRequest();
+};
+
+PolygonRequest::PolygonRequest(/* args */)
+{
+}
+
+PolygonRequest::~PolygonRequest()
+{
+}
+
+
+int main(void){
+    std::cout << "Hello world\n";
+    return 0;    
+}
+
